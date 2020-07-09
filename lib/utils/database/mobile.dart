c527b60c7f4218a704f1fbfd584e979e53fbd037
Sorunome <mail@sorunome.de>
2020-07-09 21:40:59 +0200

    switch to moor_ffi w/ sqlcipher

--- lib/utils/database/mobile.dart
+++ lib/utils/database/mobile.dart
@@ -1,14 +1,28 @@
+import 'dart:io';
 import 'package:famedlysdk/famedlysdk.dart';
-import 'package:encrypted_moor/encrypted_moor.dart';
+import 'package:sqflite/sqflite.dart' show getDatabasesPath;
+import 'package:path/path.dart' as p;
 import 'package:flutter/material.dart';
+import 'package:moor/moor.dart';
+import 'cipher_db.dart' as cipher;
+
+bool _inited = false;
 
 Database constructDb(
     {bool logStatements = false,
     String filename = 'database.sqlite',
     String password = ''}) {
+  if (!_inited) {
+    cipher.init();
+    _inited = true;
+  }
   debugPrint('[Moor] using encrypted moor');
-  return Database(EncryptedExecutor(
-      path: filename, password: password, logStatements: logStatements));
+  return Database(LazyDatabase(() async {
+    final dbFolder = await getDatabasesPath();
+    final file = File(p.join(dbFolder, filename));
+    return cipher.VmDatabaseEncrypted(file,
+        password: password, logStatements: logStatements);
+  }));
 }
 
 Future<String> getLocalstorage(String key) async {
